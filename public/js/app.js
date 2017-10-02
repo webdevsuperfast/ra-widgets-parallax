d931acab6113a54819db210c552f18c92c35aa4c
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-10-02 08:34:59 +0800

    Get background-image from data attribute

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 (function($){
     $('.parallax-window').each(function(){
         var $this = $(this);
+        $this.css('background-image', 'url('+$this.data('image')+')' );
         $this.parallax({
             speed: $this.data('speed')
         });
