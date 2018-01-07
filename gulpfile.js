08d0a6961f124b1b44c76e9db22deaa4a86ffb44
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-01-07 12:51:58 +0800

    Replaced paroller.js with parallax.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,47 @@
 var gulp = require('gulp'),
-sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-rename = require('gulp-rename'),
-concat = require('gulp-concat'),
-notify = require('gulp-notify'),
-cache = require('gulp-cache'),
-vinylpaths = require('vinyl-paths'),
-cleancss = require('gulp-clean-css'),
-cmq = require('gulp-combine-mq'),
-uglify = require('gulp-uglify'),
-foreach = require('gulp-flatmap'),
-changed = require('gulp-changed'),
-merge = require('merge-stream'),
-del = require('del');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    vinylpaths = require('vinyl-paths'),
+    cleancss = require('gulp-clean-css'),
+    cmq = require('gulp-combine-mq'),
+    uglify = require('gulp-uglify'),
+    foreach = require('gulp-flatmap'),
+    changed = require('gulp-changed'),
+    merge = require('merge-stream'),
+    del = require('del');
 
 // Vendor JS
 gulp.task('scripts', function(){
-return gulp.src([
-    'bower_components/paroller.js/dist/jquery.paroller.js'
-])
-.pipe(foreach(function(stream, file){
-    return stream
-        .pipe(changed('temp/js'))
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('temp/js'))
-}))
-.pipe(gulp.dest('public/js'))
-.pipe(notify({ message: 'Scripts task complete' }));
+    return gulp.src([
+        'node_modules/jquery-parallax.js/parallax.js'
+    ])
+    .pipe(foreach(function(stream, file){
+        return stream
+            .pipe(changed('temp/js'))
+            .pipe(uglify())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest('temp/js'))
+    }))
+    .pipe(gulp.dest('public/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Clean temp folder
 gulp.task('clean:temp', function(){
-return gulp.src('temp/*')
-.pipe(vinylpaths(del))
+    return gulp.src('temp/*')
+    .pipe(vinylpaths(del))
 });
 
 // Default task
 gulp.task('default', ['clean:temp'], function() {
-gulp.start('scripts', 'watch');
+    gulp.start('scripts', 'watch');
 });
 
 // Watch
 gulp.task('watch', function() {
-gulp.watch(['assets/js/sources/*.js'], ['scripts']);
+    gulp.watch(['assets/js/sources/*.js'], ['scripts']);
 });
