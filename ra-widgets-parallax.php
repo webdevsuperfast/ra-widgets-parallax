0f937902863fe498eb31421f886e40d3ce9ca6ae
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-01-07 13:02:52 +0800

    Changed form fields to reflect parallax.js markup, code cleanup

--- ra-widgets-parallax.php
+++ ra-widgets-parallax.php
@@ -14,6 +14,10 @@ Domain Path: /languages
 
 defined( 'ABSPATH' ) or die( esc_html_e( 'With great power comes great responsibility.', 'ra-widgets-parallax' ) );
 
+$plugin_data = get_plugin_data( __FILE__ );
+
+define( 'RAWP_VERSION', $plugin_data['Version'] );
+
 class RA_Widgets_Parallax {
     public function __construct() {
         // Add input fields
@@ -30,59 +34,56 @@ class RA_Widgets_Parallax {
 
         // Enqueue Admin scripts
         add_action( 'admin_enqueue_scripts', array( $this, 'rawp_admin_enqueue_scripts' ) );
+
+        // Enqueue SiteOrigin Panels Admin scripts
+        add_action( 'siteorigin_panel_enqueue_admin_scripts', array( $this, 'rawp_siteorigin_panels_admin_scripts' ) );
     }
 
     public function rawp_in_widget_form( $t, $return, $instance ) {
         $instance = wp_parse_args( (array) $instance, array( 
             'parallax-image' => '', 
-            'parallax-speed' => '0.3', 
-            'parallax-type' => 'background',
-            'parallax-direction' => 'vertical'
+            'parallax-speed' => '0.2', 
+            'parallax-position' => 'center center',
+            'parallax-container' => 'body'
         ) );
 
         if ( !isset( $instance['parallax-image'] ) ) $instance['parallax-image'] = null;
         if ( !isset( $instance['parallax-speed'] ) ) $instance['parallax-speed'] = null; 
-        if ( !isset( $instance['parallax-type'] ) ) $instance['parallax-type'] = null;
-        if ( !isset( $instance['parallax-direction'] ) ) $instance['parallax-direction'] = null;
+        if ( !isset( $instance['parallax-position'] ) ) $instance['parallax-position'] = null;
+        if ( !isset( $instance['parallax-container'] ) ) $instance['parallax-container'] = null;
         
-        // Parallax Type
-        $type = $this->rawp_type();
-
-        // Parallax Direction
-        $direction = $this->rawp_direction();
+        // Parallax Position
+        $position = $this->rawp_position();
         ?>
 
         <div class="rawp-fields">
-            <h3 class="toggle"><?php _e( 'Parallax Settings', 'ra-widgets-parallax' ); ?></h3>
+            <h3 class="rawp-toggle"><?php _e( 'Parallax Settings', 'ra-widgets-parallax' ); ?></h3>
             <div class="rawp-field" style="display: none;">
                 <p>
                     <label for="<?php echo $t->get_field_name( 'parallax-image' ); ?>"><?php _e( 'Image', 'ra-widgets-parallax' ); ?></label>
-                    <input type="text" class="widefat rawp-input-image" id="<?php echo $t->get_field_id( 'parallax-image' ); ?>" name="<?php echo $t->get_field_name( 'parallax-image' ); ?>" value="<?php echo $instance['parallax-image']; ?>" />
-                    <a href="#" class="rawp_upload_image_button button button-primary"><?php _e( 'Upload image', 'ra-widgets-parallax' ); ?></a>
+                    <img src="<?php if ( !empty( $instance['parallax-image'] ) ) echo $instance['parallax-image']; ?>" alt="">
+                    <input type="hidden" class="widefat rawp-input-image" id="<?php echo $t->get_field_id( 'parallax-image' ); ?>" name="<?php echo $t->get_field_name( 'parallax-image' ); ?>" value="<?php echo $instance['parallax-image']; ?>" />
+                    <button class="rawp_upload_image_button button button-primary"><?php _e( 'Upload image', 'ra-widgets-parallax' ); ?></button>
                     <span><em><?php _e( 'You must provide a path to the image to enable parallax effect.', 'ra-widgets-parallax' ); ?></em></span>
                 </p>
                 <p>
                     <label for="<?php echo $t->get_field_id('parallax-speed'); ?>"><?php _e( 'Speed', 'ra-widgets-parallax' ); ?></label>
                     <input type="text" class="widefat" id="<?php echo $t->get_field_id( 'parallax-speed' ); ?>" name="<?php echo $t->get_field_name( 'parallax-speed' ); ?>" value="<?php echo $instance['parallax-speed']; ?>" />
-                    <span><em><?php _e( 'This attribute is necessary to enable parallax scrolling effect. It sets elements offset and speed. It can be positive (0.3) or negative (-0.3). Less means slower.', 'ra-widgets-parallax' ); ?></em></span>
+                    <span><em><?php _e( 'The speed at which the parallax effect runs. 0.0 means the image will appear fixed in place, and 1.0 the image will flow at the same speed as the page content.', 'ra-widgets-parallax' ); ?></em></span>
                 </p>
                 <p>
-                    <label for="<?php echo $t->get_field_id( 'parallax-type' ); ?>"><?php _e( 'Type', 'ra-widgets-parallax' ); ?></label>
-                    <select class="widefat" id="<?php echo $t->get_field_id('parallax-type'); ?>" name="<?php echo $t->get_field_name('parallax-type'); ?>">
-                        <?php foreach( $type as $key => $value ) { ?>
-                            <option <?php selected( $instance['parallax-type'], $key ); ?>value="<?php echo $key; ?>"><?php echo $value; ?></option>
+                    <label for="<?php echo $t->get_field_id( 'parallax-position' ); ?>"><?php _e( 'Position', 'ra-widgets-parallax' ); ?></label>
+                    <select class="widefat" id="<?php echo $t->get_field_id('parallax-position'); ?>" name="<?php echo $t->get_field_name('parallax-position'); ?>">
+                        <?php foreach( $position as $key => $value ) { ?>
+                            <option <?php selected( $instance['parallax-position'], $key ); ?>value="<?php echo $key; ?>"><?php echo $value; ?></option>
                         <?php } ?>
                     </select>
-                    <span><em><?php _e( 'This attribute is optional. It has two values background and foreground.', 'ra-widgets-parallax' ); ?></em></span>
+                    <span><em><?php _e( 'This is analogous to the background-position css property. Specify coordinates as top, bottom, right, left, center, or pixel values (e.g. -10px 0px). The parallax image will be positioned as close to these values as possible while still covering the target element.', 'ra-widgets-parallax' ); ?></em></span>
                 </p>
                 <p>
-                    <label for="<?php echo $t->get_field_id( 'parallax-direction' ); ?>"><?php _e( 'Direction', 'ra-widgets-parallax' ); ?></label>
-                    <select class="widefat" id="<?php echo $t->get_field_id('parallax-direction'); ?>" name="<?php echo $t->get_field_name('parallax-direction'); ?>">
-                        <?php foreach( $direction as $key => $value ) { ?>
-                            <option <?php selected( $instance['parallax-direction'], $key ); ?>value="<?php echo $key; ?>"><?php echo $value; ?></option>
-                        <?php } ?>
-                    </select>
-                    <span><em><?php _e( 'This attribute is optional. It has two values vertical and horizontal.', 'ra-widgets-parallax' ); ?></em></span>
+                    <label for="<?php echo $t->get_field_id( 'parallax-container' ); ?>"><?php _e( 'Mirror Container', 'ra-widgets-parallax' ); ?></label>
+                    <input type="text" class="widefat" id="<?php echo $t->get_field_id( 'parallax-container' ); ?>" name="<?php echo $t->get_field_name( 'parallax-container' ); ?>" value="<?php echo $instance['parallax-container']; ?>" />
+                    <span><em><?php _e( 'The parallax mirror will be prepended into this container.', 'ra-widgets-parallax' ); ?></em></span>
                 </p>
             </div>
         </div>
@@ -93,10 +94,10 @@ class RA_Widgets_Parallax {
     }
 
     public function rawp_in_widget_form_update( $instance, $new_instance, $old_instance ) {
-        $instance['parallax-image'] = $new_instance['parallax-image'];
+        $instance['parallax-image'] = ( ! empty( $new_instance['parallax-image'] ) ) ? $new_instance['parallax-image'] : '';
         $instance['parallax-speed'] = $new_instance['parallax-speed'];
-        $instance['parallax-type'] = $new_instance['parallax-type'];
-        $instance['parallax-direction'] = $new_instance['parallax-direction'];
+        $instance['parallax-position'] = $new_instance['parallax-position'];
+        $instance['parallax-container'] = $new_instance['parallax-container'];
 
         return $instance;
     }
@@ -110,50 +111,35 @@ class RA_Widgets_Parallax {
         $widget_num = $widget_obj['params'][0]['number'];
 
         $attrs = array();
-        
-        if ( isset( $widget_opt[$widget_num]['parallax-image'] ) && !empty( $widget_opt[$widget_num]['parallax-image'] ) ) {
-            $attrs['class'] = 'parallax-window';
-
-            $styles = array();
-
-            $style = '';
-            $styles['background'] = 'url( '. $widget_opt[$widget_num]['parallax-image'] .' ) no-repeat center';
-
-            foreach( $styles as  $key => $value ) {
-                $style .= $key . ':' . $value . '; ';
-            }
-
-            $attrs['style'] = $style;
-        }
 
         // Parallax Speed
         if ( isset( $widget_opt[$widget_num]['parallax-speed'] ) && !empty( $widget_opt[$widget_num]['parallax-speed'] ) ) { 
-            $attrs['data-paroller-factor'] = $widget_opt[$widget_num]['parallax-speed']; 
+            $attrs['data-speed'] = $widget_opt[$widget_num]['parallax-speed']; 
         } else {
-            $attrs['data-paroller-factor'] = (int) '0.3';
+            $attrs['data-speed'] = (float) '0.2';
         }
 
-        // Parallax Type
-        if ( isset( $widget_opt[$widget_num]['parallax-type'] ) && !empty( $widget_opt[$widget_num]['parallax-type'] ) ) {
-            $attrs['data-paroller-type'] = $widget_opt[$widget_num]['parallax-type'];
-        }
-        
-        if ( isset( $widget_opt[$widget_num]['parallax-direction'] ) && !empty( $widget_opt[$widget_num]['parallax-direction'] ) ) {
-            $attrs['data-paroller-direction'] = $widget_opt[$widget_num]['parallax-direction'];
-        } 
+        // Parallax Position
+        if ( isset( $widget_opt[$widget_num]['parallax-position'] ) && !empty( $widget_opt[$widget_num]['parallax-position'] ) ) $attrs['data-position'] = $widget_opt[$widget_num]['parallax-position'];
 
-        
-        
+        // Parallax Mirror
+        if ( isset( $widget_opt[$widget_num]['parallax-container'] ) && !empty( $widget_opt[$widget_num]['parallax-container'] ) ) $attrs['data-mirror-container'] = $widget_opt[$widget_num]['parallax-container'];
+
+        // Parallax Image
         if ( isset( $widget_opt[$widget_num]['parallax-image'] ) && !empty( $widget_opt[$widget_num]['parallax-image'] ) ) {
-            $attr = 'style="position: relative;"><div ';
+            $attrs['class'] = 'parallax-window';
+            $attrs['data-parallax'] = 'scroll';
+            $attrs['data-image-src'] = $widget_opt[$widget_num]['parallax-image'];
+
+            $attr = ' ';
 
             foreach( $attrs as $key => $value ) {
                 $attr .= $key . '="' . $value .'" ';
             }
 
-            $attr .= '></div>';
+            $attr .= '>';
 
-            $params[0]['before_widget'] = preg_replace( '/>/D', $attr,  $params[0]['before_widget'], 1 );
+            $params[0]['before_widget'] = preg_replace( '/>$/D', $attr,  $params[0]['before_widget'], 1 );
         }
 
         return $params;
@@ -162,10 +148,8 @@ class RA_Widgets_Parallax {
     public function rawp_enqueue_scripts() {
         if ( !is_admin() ) {
             // Paroller JS
-            wp_register_script( 'rawp-parallax-js', plugin_dir_url( __FILE__ ) . 'public/js/jquery.paroller.min.js', array( 'jquery' ), null, true );
+            wp_register_script( 'rawp-parallax-js', plugin_dir_url( __FILE__ ) . 'public/js/parallax.min.js', array( 'jquery' ), RAWP_VERSION, true );
             wp_enqueue_script( 'rawp-parallax-js' );
-
-            wp_add_inline_script( 'rawp-parallax-js', 'jQuery(window).paroller();' );
 
             // Main CSS
             wp_enqueue_style( 'rawp-parallax-css', plugin_dir_url( __FILE__ ) . 'public/css/app.css' );
@@ -178,29 +162,32 @@ class RA_Widgets_Parallax {
 
         //Only load if we are not on the widget page
         if ( $current_page->id === 'widgets' ){
+            wp_enqueue_script( 'media-upload' );
             wp_enqueue_media();
+            
             wp_enqueue_script( 'rawp-admin-js', plugin_dir_url( __FILE__ ) . 'admin/js/admin.js' );
 
             wp_enqueue_style( 'rawp-admin-css', plugin_dir_url( __FILE__ ) . 'admin/css/admin.css' );
         }
     }
 
-    private function rawp_direction() {
-        $direction = array(
-            'horizontal' => __( 'Horizontal' ),
-            'vertical' => __( 'Vertical' )
-        );
-
-        return $direction;
+    public function rawp_siteorigin_panels_admin_scripts() {
+        wp_register_script( 'rawp-siteorigin-panels-js', plugin_dir_url( __FILE__ ) . 'admin/js/siteorigin.js', array( 'jquery' ), null, true );
+        wp_enqueue_script( 'rawp-siteorigin-panels-js' );
     }
-
-    private function rawp_type() {
-        $type = array(
-            'background' => __( 'Background' ),
-            'foreground' => __( 'Foreground' )
+    
+    private function rawp_position() {
+        $position = array(
+            'center center' => __( 'Center Center' ),
+            'center top' => __( 'Center Top' ),
+            'center bottom' => __( 'Center Bottom' ),
+            'right top' => __( 'Right Top' ),
+            'right bottom' => __( 'Right Bottom' ),
+            'left top' => __( 'Left Top' ),
+            'left bottom' => __( 'Left Bottom' )
         );
 
-        return $type;
+        return $position;
     }
 }
 
